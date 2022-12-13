ec082a9640169628adc3eb9012876c0c3f51a867
alekseichernyshev <adanayrel@mail.ru>
2022-12-13 19:44:46 +0900

    Finish first task

--- src/week1/Task1.java
+++ src/week1/Task1.java
@@ -1,4 +1,23 @@
 package week1;
 
-public class Task1
+import java.util.Scanner;
+
+// перенести числа
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner numbers = new Scanner(System.in);
+        int a = numbers.nextInt();
+        int b = numbers.nextInt();
+        int c = numbers.nextInt();
+
+        System.out.println("a = " + a + " b = " + b + " c + " + c);
+
+        int temp = c;
+        c = b;
+        b = a;
+        a = temp;
+
+        System.out.println("a = " + a + " b = " + b + " c + " + c);
+
+    }
 }
