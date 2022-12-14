9ab592e22dfa328104c9eda9856ac65a1b12d75a
alekseichernyshev <adanayrel@mail.ru>
2022-12-15 02:47:06 +0900

    Номера вторая  версия

---
+++ src/week2/Task4_2.java
@@ -0,0 +1,30 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Task4_2 {
+    public static void main(String[] args) {
+        final String rubleSignature = " rub.";
+        Scanner console = new Scanner(System.in);
+
+        int roomType = console.nextInt();
+
+        switch (roomType){
+            case 1:
+                System.out.println("Vip " + 12500 + rubleSignature);
+                break;
+            case 2:
+                System.out.println("Premium " + 9000 + rubleSignature);
+                break;
+                case 3:
+                    System.out.println("Standard " + 6300 + rubleSignature);
+                    break;
+            case 4:
+                System.out.println("Economy " + 3800 + rubleSignature);
+                break;
+        }
+
+
+
+    }
+}
