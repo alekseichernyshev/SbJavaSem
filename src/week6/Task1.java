e5d6142f0532d3e7e9d9de676efb86880fcd5497
alekseichernyshev <adanayrel@mail.ru>
2023-01-19 15:45:58 +0900

    week 6 Task1.2

--- src/week6/Task1.java
+++ src/week6/Task1.java
@@ -1,7 +1,28 @@
 package week6;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        int m = console.nextInt();
 
+        int[][] array = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array[i][j] = console.nextInt();
+            }
+        }
+        int[] res = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i] += array[j][i];
+            }
+        }
+        System.out.println(Arrays.toString(res));
     }
 }
