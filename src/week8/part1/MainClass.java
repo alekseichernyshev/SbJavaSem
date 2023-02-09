03bbe7bd2867163f25fc0d8add1d062a88ae4c70
alekseichernyshev <adanayrel@mail.ru>
2023-02-09 18:53:22 +0900

    enum

--- src/week8/part1/MainClass.java
+++ src/week8/part1/MainClass.java
@@ -1,4 +1,14 @@
 package week8.part1;
 
 public class MainClass {
+    public static void main(String[] args) {
+        //Task1
+        //System.out.println(WeekDays.MONDAY);
+
+        String name = "среда";
+        System.out.println("Порядковый номер дня недели: " + name + " = " + WeekDays.ofName(name).getDayNumber());
+        int number = 7;
+        System.out.println("Порядковый номер дня недели: " + number + " = " + WeekDays.ofNumber(number).getName());
+
+    }
 }
--- src/week8/part1/WeekDays.java
+++ src/week8/part1/WeekDays.java
@@ -1,4 +1,53 @@
 package week8.part1;
 
-public class WeekDays {
+public enum WeekDays {
+    MONDAY(1, "понедельник"),
+    TUESDAY(2, "вторник"),
+    WEDNESDAY(3, "среда"),
+    THURSDAY(4, "четверг"),
+    FRIDAY(5, "пятницв"),
+    SATURDAY(6, "суббота"),
+    SUNDAY(7, "воскресенье");
+
+    private final int dayNumber;
+    private final String name;
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    WeekDays(int dayNumber, String name){
+        this.dayNumber = dayNumber;
+        this.name = name;
+    }
+
+    public static WeekDays ofNumber(int dayNumber){
+        for (WeekDays ownershipType:values()){
+            if(ownershipType.dayNumber == dayNumber){
+                return ownershipType;
+            }
+        }
+        throw new IllegalArgumentException("Неизвестное значение номера дня недели: " +dayNumber);
+    }
+
+    public static WeekDays ofName(String name){
+        for (WeekDays ownershipType:values()){
+            if(ownershipType.name.equals(name)){
+                return ownershipType;
+            }
+        }
+        throw new IllegalArgumentException("Неизвестное значение имени дня недели: " +name);
+    }
+
+    @Override
+    public String toString() {
+        return "WeekDays{" +
+                "dayNumber=" + dayNumber +
+                ", name='" + name + '\'' +
+                '}' + super.toString();
+    }
 }
