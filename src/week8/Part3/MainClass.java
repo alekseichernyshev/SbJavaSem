d3b02dbd9b2b0e4b64a2b9cf9fe22811f78ec766
alekseichernyshev <adanayrel@mail.ru>
2023-02-09 22:46:23 +0900

    Array

--- src/week8/Part3/MainClass.java
+++ src/week8/Part3/MainClass.java
@@ -2,10 +2,17 @@ package week8.Part3;
 
 public class MainClass {
     public static void main(String[] args) {
-        SimpleArrayList myArray = new SimpleArrayList();
-        myArray.add(12);
-        myArray.add(99);
-        System.out.println("List size: " + myArray.size());
-        System.out.println("Element with first index: " + myArray.get(1));
+//        SimpleArrayList myArray = new SimpleArrayList();
+//        myArray.add(12);
+//        myArray.add(99);
+//        System.out.println("List size: " + myArray.size());
+//        System.out.println("Element with first index: " + myArray.get(1));
+
+        SimpleLinkedList myList = new SimpleLinkedList();
+        myList.addAtHead(12);
+        myList.addAtHead(50);
+        myList.addAtTail(99);
+
+        System.out.println(myList.get(1));
     }
 }
--- src/week8/Part3/SimpleLinkedList.java
+++ src/week8/Part3/SimpleLinkedList.java
@@ -1,4 +1,65 @@
 package week8.Part3;
 
 public class SimpleLinkedList {
+    private Node head;
+    private int size;
+
+    private static class Node {
+        int value;
+        Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public SimpleLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int idx) {
+        if (idx<0|| idx>=size){
+            return  -1;
+        }
+        Node node = head;
+        for (int i = 0; i<idx; i++){
+            node = node.next;
+        }
+        return node.value;
+    }
+
+    public void addAtHead(int value) {
+        Node newHead = new Node(value, head );
+        head = newHead;
+        size++;
+    }
+
+    public void addAtTail(int value) {
+        if (size>0){
+            Node node = head;
+            for (int i = 0; i<size-1; i++){
+               node = node.next;
+            }
+            node.next = new Node(value, null);
+        }else {
+            head = new Node(value, null);
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int value){
+
+    }
+
+    public void deleteAtIndex(int index){
+
+    }
+
+    public int size(){
+        return size;
+    }
+
+
 }
